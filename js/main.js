f1a970b5522772aa289ebcf3f39fc519c14370c7
Alok Shukla <shuklaalok7@gmail.com>
2015-11-15 22:44:49 +0530

    A little more content License text is missing from each and every file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var DEV_MODE = true;
-var DEV_MODE_APPLIED = DEV_MODE && location.href.indexOf("localhost")>-1;
+var DEV_MODE_APPLIED = DEV_MODE && location.href.indexOf("localhost")==-1;
 
 $(document).ready(function () {
     var body = $('body');
@@ -9,8 +9,11 @@ $(document).ready(function () {
         $(this).html(now.getFullYear());
     });
 
-    if(!DEV_MODE_APPLIED) {
-        body.find('.site-content').html("We are building it up for you.");
+    if(DEV_MODE_APPLIED) {
+        //body.find('.site-content').html("We are building it up for you.");
+        body.find('.under-development').each(function() {
+            $(this).hide();
+        });
     }
 
 });
