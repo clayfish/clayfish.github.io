2df08779d3d132537e600bd71711261016c26a2a
Alok Shukla <shuklaalok7@gmail.com>
2015-11-15 19:33:40 +0530

    social media links License and a little text

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var DEV_MODE = true;
+var DEV_MODE_APPLIED = DEV_MODE && location.href.indexOf("localhost")>-1;
 
 $(document).ready(function () {
     var body = $('body');
@@ -8,7 +9,7 @@ $(document).ready(function () {
         $(this).html(now.getFullYear());
     });
 
-    if(DEV_MODE) {
+    if(!DEV_MODE_APPLIED) {
         body.find('.site-content').html("We are building it up for you.");
     }
 
